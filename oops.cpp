3efd38ea658e0257b3c27a1c07b2ef77a755d114
Nizam6239 <nizammalik6239@gmail.com>
2024-06-21 17:21:32 +0530

    Starting the basic of example of teacher

---
+++ oops.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher{
+    //properties or attributes
+    public:
+    string name;
+    string dept;
+    string subject;
+    double salary;
+
+    //methods or member Functions
+    void changeDept(string newDept){
+        dept = newDept;
+    }
+};
+
+int main(){
+    Teacher t1;
+    t1.name = "Nizam Malik";
+    t1.dept = "Computer Science";
+    t1.subject = "C++";
+    t1.salary = 25000;
+    cout<<"Name : "<< t1.name<<endl;
+    cout<<"Department : " <<t1.dept<<endl;
+    cout<<"Subject : "<<t1.subject<<endl;
+    cout<<"Salary : "<<t1.salary<<endl; 
+    return 0;
+}

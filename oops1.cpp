a409a24b1e7168f51c0a9b1dcf60a5cef54affad
Nizam6239 <nizammalik6239@gmail.com>
2024-06-22 19:26:02 +0530

    Destructor to oop1

--- oops1.cpp
+++ oops1.cpp
@@ -13,23 +13,19 @@ class Student{
         *cgpaPtr = cgpa;
     }
 
-    Student(Student &obj){
-        this->name = obj.name;
-        cgpaPtr = new double;
-        *cgpaPtr = *obj.cgpaPtr;
-    }
-
     void getInfo(){
         cout<<"Name : "<< name <<endl;
         cout<<"cgpa : "<< *cgpaPtr <<endl;
+    }
+
+    ~Student(){
+        cout<< "I am deleting the everything."<<endl;
+        delete cgpaPtr;
     }
 };
 
 int main(){
     Student s1("Nizam Malik", 8.50);
-    Student s2(s1);
-    s1.getInfo();
-    *(s2.cgpaPtr) = 9.2;
     s1.getInfo();
     return 0;
 }
